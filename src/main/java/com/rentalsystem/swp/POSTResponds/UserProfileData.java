492c95a669d46d5155f9862d4c731c449f3544b2
Dapimex <denis-pimenov2011@yandex.ru>
2019-04-01 02:39:46 +0300

    Menu template and style, getters and setters asked by system

--- src/main/java/com/rentalsystem/swp/POSTResponds/UserProfileData.java
+++ src/main/java/com/rentalsystem/swp/POSTResponds/UserProfileData.java
@@ -10,4 +10,52 @@ public class UserProfileData {
     private String email;
     private String password;
     private String phoneNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
