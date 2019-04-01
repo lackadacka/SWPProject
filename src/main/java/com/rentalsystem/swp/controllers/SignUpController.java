b5e98f60943c2b10d719dbcce0b51b216fc27e0a
beg1shev <begishev1998@mail.ru>
2019-04-02 00:59:17 +0300

    Added signup page

--- src/main/java/com/rentalsystem/swp/controllers/SignUpController.java
+++ src/main/java/com/rentalsystem/swp/controllers/SignUpController.java
@@ -1,18 +1,18 @@
 package com.rentalsystem.swp.controllers;
 
-import com.rentalsystem.swp.POSTResponds.LoginData;
 import com.rentalsystem.swp.dao.UserProfile;
 import com.rentalsystem.swp.POSTResponds.UserProfileData;
 import com.rentalsystem.swp.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-@RestController
+@Controller
 public class SignUpController {
 
     private final UserRepository userRepository;
@@ -23,13 +23,27 @@ public class SignUpController {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
+
+//    @GetMapping("/signup")
+//    public String signUp(){
+//        return "signup";
+//    }
+//    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+//    public String showSignup(Model model) {
+//        model.addAttribute("UserProfileData", new UserProfileData());
+//        return "signup";
+//    }
+
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public String showLogin(Model model) {
-        model.addAttribute("", new LoginData());
+    public String showSignup(Model model){
+        model.addAttribute("userProfileData", new UserProfileData());
         return "signup";
     }
-    @RequestMapping("/signup")
-    public void addNewUser(@ModelAttribute("") UserProfileData user, Model model) {
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signUp(@ModelAttribute("userProfileData") UserProfileData userProfileData, Model model){
+
+        model.addAttribute("UserProfileData", userProfileData);
+        UserProfile user = new UserProfile(userProfileData.getFirstName(), userProfileData.getLastName(), userProfileData.getEmail(),userProfileData.getPassword(),userProfileData.getPhoneNumber());
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email address already exists.");
         }
@@ -39,14 +53,28 @@ public class SignUpController {
         newUser.setLastName(user.getLastName());
         newUser.setEmail(user.getEmail());
         newUser.setPhoneNumber(user.getPhoneNumber());
-
-        newUser.setPassword(user.getPassword());
-        userRepository.save(newUser);
+        return "login";
     }
+//    @GetMapping("/signup")
+//    public void addNewUser(UserProfileData user) {
+//        if (userRepository.existsByEmail(user.getEmail())) {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email address already exists.");
+//        }
+//
+//        UserProfile newUser = new UserProfile();
+//        newUser.setFirstName(user.getFirstName());
+//        newUser.setLastName(user.getLastName());
+//        newUser.setEmail(user.getEmail());
+//        newUser.setPhoneNumber(user.getPhoneNumber());
+//
+//        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+//        userRepository.save(newUser);
+//    }
 
     @GetMapping(path="/allusers")
     public @ResponseBody Iterable<UserProfile> getAllUsers() {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
     }
+
 }
