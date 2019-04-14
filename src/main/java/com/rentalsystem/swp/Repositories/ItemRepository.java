11f7f96849ea93f8ce75afd89cce7537b21f5ee4
Dapimex <denis-pimenov2011@yandex.ru>
2019-04-14 23:47:03 +0300

    ItemRepository new method

--- src/main/java/com/rentalsystem/swp/Repositories/ItemRepository.java
+++ src/main/java/com/rentalsystem/swp/Repositories/ItemRepository.java
@@ -4,6 +4,10 @@ import com.rentalsystem.swp.dao.ItemProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ItemRepository extends JpaRepository<ItemProfile, Integer> {
+    List<ItemProfile> findAllByOwnerIs(String owner);
 }
